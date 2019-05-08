835a8b1682b8d734d96b5119ae1687a09f776d1c
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2019-05-08 11:15:35 +0300

    Fix dummy sql

--- sql-data/dummy.sql
+++ sql-data/dummy.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS `persons`;
 CREATE TABLE persons (
     id int PRIMARY KEY,
     name varchar(255),
-    email varchar(255) DEFAULT Null,
+    email varchar(255) DEFAULT Null
 );
