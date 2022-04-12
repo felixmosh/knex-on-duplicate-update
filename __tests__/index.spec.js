998563442671e143a7aa2ea1f008312527f25956
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2022-04-12 10:26:23 +0300

    Add prettier

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -15,9 +15,8 @@ const db = knex({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-  }
+  },
 });
-
 
 function getById(id) {
   return db('persons').where({ id }).first();
@@ -30,37 +29,39 @@ describe('onDuplicateUpdate', () => {
 
   describe('validation', () => {
     it('should throw if applied to none insert query', () => {
-      expect(() => db('persons').select('id').onDuplicateUpdate(''))
-        .toThrowError('onDuplicateUpdate error: should be used only with insert query.');
+      expect(() => db('persons').select('id').onDuplicateUpdate('')).toThrowError(
+        'onDuplicateUpdate error: should be used only with insert query.'
+      );
     });
 
     it('should throw if no columns given', () => {
-      expect(() => db.insert({ id: 1, name: 'test' }).into('persons').onDuplicateUpdate())
-        .toThrowError('onDuplicateUpdate error: please specify at least one column name.');
+      expect(() =>
+        db.insert({ id: 1, name: 'test' }).into('persons').onDuplicateUpdate()
+      ).toThrowError('onDuplicateUpdate error: please specify at least one column name.');
     });
     it('should throw if columns are not string or object', () => {
-      expect(() => db.insert({ id: 1, name: 'test' }).into('persons').onDuplicateUpdate(false))
-        .toThrowError('onDuplicateUpdate error: expected column name to be string or object.');
+      expect(() =>
+        db.insert({ id: 1, name: 'test' }).into('persons').onDuplicateUpdate(false)
+      ).toThrowError('onDuplicateUpdate error: expected column name to be string or object.');
     });
     it('should throw if insert is empty array', () => {
-      expect(() => db.insert([]).into('persons').onDuplicateUpdate('whatever'))
-        .toThrowError('onDuplicateUpdate error: empty insert statement.');
+      expect(() => db.insert([]).into('persons').onDuplicateUpdate('whatever')).toThrowError(
+        'onDuplicateUpdate error: empty insert statement.'
+      );
     });
   });
 
   describe('behaviour', () => {
     it('should allow insert new row', async () => {
       const person = { id: 2, name: 'test' };
-      await db.insert(person).into('persons')
-        .onDuplicateUpdate('name');
+      await db.insert(person).into('persons').onDuplicateUpdate('name');
       const insertPerson = await getById(2);
       expect(insertPerson).toEqual(expect.objectContaining(person));
     });
 
     it('should update on duplicate', async () => {
       await db.insert({ id: 3, name: 'test3' }).into('persons');
-      await db.insert({ id: 3, name: 'test33' }).into('persons')
-        .onDuplicateUpdate('name');
+      await db.insert({ id: 3, name: 'test33' }).into('persons').onDuplicateUpdate('name');
       const person = await getById(3);
 
       expect(person.name).toBe('test33');
@@ -69,11 +70,13 @@ describe('onDuplicateUpdate', () => {
     it('should allow updating multiple columns', async () => {
       const id = 4;
       await db.insert({ id, name: 'test4', email: '1@1.com' }).into('persons');
-      await db.insert({
-        id,
-        name: 'test5',
-        email: '5@5.com'
-      }).into('persons')
+      await db
+        .insert({
+          id,
+          name: 'test5',
+          email: '5@5.com',
+        })
+        .into('persons')
         .onDuplicateUpdate('name', 'email');
       const person = await getById(id);
 
@@ -83,45 +86,48 @@ describe('onDuplicateUpdate', () => {
     it('should allow specifying a value for a column when updating multiple', async () => {
       const id = 5;
       await db.insert({ id, name: 'test6', email: '6@6.com' }).into('persons');
-      await db.insert({
-        id,
-        name: 'test6',
-        email: '6@6.com'
-      }).into('persons')
-        .onDuplicateUpdate({ 'email': 'updated-email', 'name': 'update-name' });
+      await db
+        .insert({
+          id,
+          name: 'test6',
+          email: '6@6.com',
+        })
+        .into('persons')
+        .onDuplicateUpdate({ email: 'updated-email', name: 'update-name' });
 
       const person = await getById(id);
-      expect(person).toEqual(expect.objectContaining({
-        name: 'update-name',
-        email: 'updated-email',
-      }));
+      expect(person).toEqual(
+        expect.objectContaining({
+          name: 'update-name',
+          email: 'updated-email',
+        })
+      );
     });
 
     it('should allow specifying a value for a column when updating multiple and mix types', async () => {
       const id = 6;
       await db.insert({ id, name: 'test6', email: '6@6.com' }).into('persons');
-      await db.insert({
-        id,
-        name: 'test6',
-        email: '6@6.com'
-      }).into('persons')
-        .onDuplicateUpdate({ 'email': 'updated-email' }, 'name');
+      await db
+        .insert({
+          id,
+          name: 'test6',
+          email: '6@6.com',
+        })
+        .into('persons')
+        .onDuplicateUpdate({ email: 'updated-email' }, 'name');
 
       const person = await getById(id);
-      expect(person).toEqual(expect.objectContaining({
-        name: 'test6',
-        email: 'updated-email',
-      }));
+      expect(person).toEqual(
+        expect.objectContaining({
+          name: 'test6',
+          email: 'updated-email',
+        })
+      );
     });
 
     it('should correctly escape field with ? character', async () => {
-      await db
-        .insert({ id: 7, name: 'other value' })
-        .into('persons');
-      await db
-        .insert({ id: 7, name: '?' })
-        .into('persons')
-        .onDuplicateUpdate('name');
+      await db.insert({ id: 7, name: 'other value' }).into('persons');
+      await db.insert({ id: 7, name: '?' }).into('persons').onDuplicateUpdate('name');
       const person = await getById(7);
 
       expect(person.name).toBe('?');
